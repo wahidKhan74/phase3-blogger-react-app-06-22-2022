201fa5da3f29dde8b31185f7fcdd9adda05dd03b
Waheed <wahid.khan74@gmail.com>
2022-06-24 07:19:45 +0000

    added components example

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -4,12 +4,13 @@ export default class ProductForm extends Component {
 
     constructor() {
         super();
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
         this.state = {
             product: {
                 id: 0,
                 title: '',
                 body: '',
-                author: '',
+                price: '',
                 category: ''
             },
             categories: [
@@ -19,6 +20,34 @@ export default class ProductForm extends Component {
                 { code: 'BOOK', name: 'Books' }
             ],
         }
+    }
+
+    // handle form submit
+    handleFormSubmit(event) {
+        event.preventDefault();
+        console.log("This : ", this);
+        
+    }
+
+    handleTitleChange = (e) => {
+        const product = this.state.product;
+        this.setState({ product: { ...product, title: e.target.value } });
+    }
+
+    handleBodyChange = (e) => {
+        const product = this.state.product;
+        this.setState({ product: { ...product, body: e.target.value } });
+    }
+
+
+    handlePriceChange = (e) => {
+        const product = this.state.product;
+        this.setState({ product: { ...product, price: e.target.value } });
+    }
+
+    handleCategoryChange = (e) => {
+        const product = this.state.product;
+        this.setState({ product: { ...product, category: e.target.value } });
     }
 
     renderCategories() {
@@ -33,24 +62,26 @@ export default class ProductForm extends Component {
             <h3>Create Product Form</h3>
             <div className="card bg-light">
             <div className="card-body">
-                <form >
+                <form onSubmit={this.handleFormSubmit}>
                 <div className="form-group">
                     <label htmlFor="title">Title</label>
-                    <input type="text" className="form-control" id="title" name="title"
-                    placeholder="Enter title" value={this.state.product.title}  />
+                    <input type="text" className="form-control" id="title" name="Enter Product title"
+                    placeholder="Enter title" value={this.state.product.title}  onChange={this.handleTitleChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="body">Body</label>
-                    <textarea className="form-control" id="body" name="body" placeholder="Enter body"
-                    rows="3" cols="30" value={this.state.product.body}></textarea>
+                    <textarea className="form-control" id="body" name="body" placeholder="Enter decription"
+                    rows="3" cols="30" value={this.state.product.body} onChange={this.handleBodyChange}></textarea>
                 </div>
                 <div className="form-group">
                     <label htmlFor="price">Price</label>
-                    <input type="text" className="form-control" id="price" name="price" placeholder="Enter author" value={this.state.product.price}  />
+                    <input type="number" className="form-control" id="price" name="price" placeholder="Enter price" 
+                    value={this.state.product.price}  onChange={this.handlePriceChange} />
                 </div>
                 <div className="form-group">
                     <label htmlFor="category">Category</label>
-                    <select required className="form-control" id="category" name="category" value={this.state.product.category}>
+                    <select required className="form-control" id="category" name="category" 
+                    value={this.state.product.category} onChange={this.handleCategoryChange}>
                     <option value=""></option>
                     {this.renderCategories()}
                     </select>
